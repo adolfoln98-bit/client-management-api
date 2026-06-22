9c2094554bf65f863ad079cbdf7915cb441cc73f
Adolfo Lopez <adolfoln98@gmail.com>
2026-06-22 18:52:58 +0200

    refactor: clean auth routes

--- routes/auth.py
+++ routes/auth.py
@@ -1,26 +1,20 @@
 from fastapi import APIRouter, HTTPException
-from models import (
-    UsuarioCreate, 
-    UsuarioOut, 
-    UsuarioLogin, 
-    Token
-    )
-from services.clientes_service import (
-    registrar_usuario,
-    login
-    )
 
-auth_router= APIRouter(prefix="/auth", tags=["auth"])
+from models import Token, UsuarioCreate, UsuarioLogin, UsuarioOut
+from services.clientes_service import login, registrar_usuario
+
+
+auth_router = APIRouter(prefix="/auth", tags=["auth"])
 
 
 @auth_router.post("/register", response_model=UsuarioOut)
-def register (usuario: UsuarioCreate):
+def register(usuario: UsuarioCreate):
     try:
         return registrar_usuario(usuario)
     except ValueError as error:
-        raise HTTPException(status_code=400, detail= str(error))
+        raise HTTPException(status_code=400, detail=str(error))
 
-    
+
 @auth_router.post("/login", response_model=Token)
 def user_login(usuario: UsuarioLogin):
     try:
