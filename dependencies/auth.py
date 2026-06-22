fd6cb917a6cc3d028d8715a6b63a28d4883ad6f5
Adolfo Lopez <adolfoln98@gmail.com>
2026-06-22 18:50:12 +0200

    refactor: clean admin dependency

--- dependencies/auth.py
+++ dependencies/auth.py
@@ -7,7 +7,6 @@ from security import(
 
 def requerir_admin(usuario_actual = Depends(get_usuario_actual)):
     try:
-        print(usuario_actual)
         return get_admin_user(usuario_actual)
     except PermissionError as error:
         raise HTTPException(status_code=403, detail=str(error))
