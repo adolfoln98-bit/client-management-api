87fac1146da1f65a5df93bc37047f25a42984ff6
Adolfo Lopez <adolfoln98@gmail.com>
2026-06-23 12:27:31 +0200

    refactor: clean pydantic models

--- models.py
+++ models.py
@@ -1,19 +1,24 @@
-from pydantic import BaseModel, EmailStr, Field, field_validator
 from enum import Enum
+
+from pydantic import BaseModel, EmailStr, Field, field_validator
+
 
 class ClienteBase(BaseModel):
     nombre: str = Field(..., min_length=1, max_length=50)
     edad: int = Field(..., ge=0, le=120)
+
     @field_validator("nombre")
     @classmethod
-    def validar_nombre(cls, value: str):
-        value= value.strip()
+    def validar_nombre(cls, value: str) -> str:
+        value = value.strip()
         if not value:
-            raise ValueError("El nombe debe tener al menos un caracter")
+            raise ValueError("El nombre debe tener al menos un carácter")
         return value
+
 
 class ClienteOut(ClienteBase):
     id: int
+
 
 class ClientesResponse(BaseModel):
     data: list[ClienteOut]
@@ -23,35 +28,42 @@ class ClientesResponse(BaseModel):
     page: int
     total_pages: int
 
+
 class UsuarioBase(BaseModel):
     email: EmailStr
+
 
 class UsuarioCreate(UsuarioBase):
     password: str = Field(..., min_length=8)
 
     @field_validator("password")
     @classmethod
-    def validar_password(cls, value: str):
+    def validar_password(cls, value: str) -> str:
         if not any(caracter.isalpha() for caracter in value):
             raise ValueError("La contraseña debe tener al menos una letra")
         if not any(caracter.isdigit() for caracter in value):
-            raise ValueError("La contraseña debe tener al menos un numero")
-        
+            raise ValueError("La contraseña debe tener al menos un número")
+
         return value
+
 
 class UsuarioOut(UsuarioBase):
     id: int
 
+
 class UsuarioLogin(UsuarioBase):
     password: str
+
 
 class Token(BaseModel):
     access_token: str
     token_type: str
 
+
 class OrderBy(str, Enum):
     asc = "asc"
     desc = "desc"
+
 
 class SortBy(str, Enum):
     id = "id"
